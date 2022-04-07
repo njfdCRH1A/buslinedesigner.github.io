84e7a463dd1f5bcda34991f8919160fe06fcc9b1
BobLiu <bobliu@bobliu.tech>
2022-04-07 10:37:32 +0800

    Remove version description

--- components/TabAbout.js
+++ components/TabAbout.js
@@ -11,10 +11,6 @@ app.component('tab-about', {
         <p class="lead"><b>也欢迎关注我的<a class="text-reset" href="https://space.bilibili.com/273163717" target="_blank">哔哩哔哩账号</a>。</b></p>
         <br />
         <h1 class="fw-normal display-5">说明</h1>
-        <p class="lead"><b>Version 1.0 Beta</b></p>
-        <ul>
-            <li>Bus Line Deisnger 公测发布</li>
-        </ul>
         <p class="lead"><b>效果不符合预期但可能无法修复的“特性”：</b></p>
         <ul>
             <li>#01 选择“多站台车站”时可能无法识别车站名或识别到附近其他车站名（高德地图数据储存问题）</li>
