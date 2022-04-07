4a6597307cb96e43b2f1a2b3c80b4e1886924afa
BobLiu <bobliu@bobliu.tech>
2022-04-07 16:16:16 +0800

    Fix bugs

--- components/TabStation.js
+++ components/TabStation.js
@@ -104,7 +104,7 @@ app.component('tab-station', {
                         <button type="button" class="btn btn-outline-primary" :class="{ active: selectedDirection == 'down' }" @click="setDirection('down')">下行</button>
                     </div>
                     <div class="btn-group btn-group-sm pull-right" role="group" style="float:right">
-                        <button type="button" class="btn btn-outline-primary" :class="{ active: showStationsOnly }" title="只显示站点" @click="setShowStationsOnly()">
+                        <button type="button" class="btn btn-outline-primary" :class="{ active: showStationsOnly }" title="只显示站点" @click="setShowStationsOnly(null)">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-record-circle" viewBox="0 0 16 16">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                 <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
